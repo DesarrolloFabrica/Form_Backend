99d8eb713f77136590eb2563e69017f4d7c9ce42
DesarrolloFabrica <desarrollofabrica@cun.edu.co>
2026-04-10 15:45:10 -0500

    xd

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
